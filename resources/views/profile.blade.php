6b687a3cbf99e1c1fcccdc3b027c90713dc8345a
hotsaucejake <get@hotsaucejake.com>
2017-03-03 03:31:33 +0000

    update user profile page
    - Reason(s):
    
    - Change(s):
      - create resources/views/layouts/profile.blade.php
      - edit resources/views/profile.blade.php
      - create show function in ProfileController
    
    - Reference(s):

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.profile')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Profile</div>
+                <div class="panel-heading">Hollas</div>
 
                 <div class="panel-body">
-                    {{ $username }}
+                    No hollas
                 </div>
             </div>
         </div>
